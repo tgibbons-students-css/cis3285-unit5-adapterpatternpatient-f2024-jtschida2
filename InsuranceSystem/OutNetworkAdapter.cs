40557e8cfa2aaf01fe5cf5c1c64c398bbf4e5941
Joe <jtschida2@css.edu>
2024-10-06 12:14:10 -0500

    All changes done for this week

---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+        public OutNetworkAdapter(string newName, int newPolicyNumber) {
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            decimal coveragePercent = patient.CoveragePercent(ProcedureCost);
+            return (coveragePercent * ProcedureCost);
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+        }
+
+        public string getPolicyNumber()
+        {
+            return patient.PolicyNumber.ToString();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+            string covered = patient.IsCovered(patientName, Int32.Parse(policyNumber));
+            if (covered.Equals("yes"))
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
